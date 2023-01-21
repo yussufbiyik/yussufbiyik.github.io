2fbfcb255c659bcca1e74dbd3068cb302a9bef54
Yusuf Bıyık <yusufbyk3660@gmail.com>
2023-01-21 23:24:28 +0300

    Add partial language support
    note:
    support is only partial because it only covers the right side
    left side will be added in the next commit

--- app.js
+++ app.js
@@ -23,140 +23,20 @@ document.addEventListener('mouseout', (event) => {
 
 const contentHeading = document.getElementById("content-heading");
 const contentDetails = document.getElementById("content-details");
-const pages = [
-    {
-        name:"projelerim",
-        heading: "Projelerim",
-        contents: [
-            {
-                background: './assets/musicflake.webp',
-                name: "Musicflake",
-                summary: "Open-Mateo.com'un API'ı ile konumuzdaki hava durumunu tespit edip hava durumuna göre Spotify'dan çalma listesi öneren bir Angular projesi.",
-                actions: [
-                    {
-                        name: "Kaynak Kod",
-                        link: "https://github.com/yussufbiyik/musicflake"
-                    },
-                    {
-                        name: "Demo",
-                        link: "https://musicflake-3f0a8.web.app/"
-                    }
-                ]
-            },
-            {
-                background:'./assets/otoroblox.webp',
-                name:'OtoRoblox',
-                summary: "Kullanıcılarının oyun içi ödüller kazandığı bir uygulamanın ödül dağıtım yazılımıdır, bir API ile oyundaki (Roblox) grubun kullanıcıları arasından görevlerini tamamlayanlara ödüllerini verir.",
-                actions: []
-            },
-            {
-                background:'./assets/periyodik-tablo.webp',
-                name:'Periyodik Tablo',
-                summary: "Bir müşterim için sonradan geliştirdiğim bir mobil uygulamanın basit bir prototipi, sonraki aşamada prototipi Angular'a uyarlayıp Cordova ile bir mobil uygulamaya çevirdim.",
-                actions: [
-                    {
-                        name: "Kaynak Kod",
-                        link: "https://github.com/yussufbiyik/periodic-table"
-                    },
-                    {
-                        name: "Demo",
-                        link: "https://yussufbiyik.github.io/periodic-table/"
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        name: "yetkinliklerim",
-        heading: "Yetkinliklerim",
-        content: `
-            <div class="skills">
-                <div id="software-related" class="skill">
-                    <ul>
-                        <li>
-                            <span>Web Geliştirme</span>
-                            <ul>
-                                <li>
-                                    <span>HTML & CSS</span>
-                                    <ul>UIkit</ul>
-                                </li>
-                            </ul>
-                            <ul>
-                                <li>
-                                    <span>Javascript & Typescript</span>
-                                    <ul>NodeJS, Angular, Electron, React Native</ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>Python</li>
-                        <li>Arduino</li>
-                        <li>Firebase</li>
-                        <li>SQL</li>
-                    </ul>
-                </div>
-                <div id="general-skills" class="skill">
-                    <ul>
-                        <li>
-                            <span>3D</span>
-                            <ul>
-                                <li>Cinema 4D (Modelleme ve Animasyon)</li>
-                                <li>Tinkercad</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <span>2D</span>
-                            <ul>
-                                <li>Photoshop</li>
-                                <li>Aseprite (Pixel Art)</li>
-                                <li>Video Düzenleme</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>`
-    },
-    {
-        name: "pixel-art",
-        heading: "Pixel Art",
-        contents: [
-            "https://steamuserimages-a.akamaihd.net/ugc/1689373781825613769/6D86A60AD7424F0D53E00E1420C3E3D3AE3CE327/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-            "https://steamuserimages-a.akamaihd.net/ugc/1633076066618028298/F53768ED0B0B5CA8852B43C3F498204B7C39693C/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-            "https://steamuserimages-a.akamaihd.net/ugc/1474317354726727080/3AA259DF9EA002FB45D128CB02B185BADD7FEA40/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-            "https://steamuserimages-a.akamaihd.net/ugc/1689373781826880823/189E3DBD5E9E0DEF1C3BAB1112722EB9327E6DDD/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-            "https://steamuserimages-a.akamaihd.net/ugc/1813239499466346854/614AD40A4B7E3B08EF7AFBCD5149C87CC18DD630/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-            "https://steamuserimages-a.akamaihd.net/ugc/1691625928964912516/F6F49F0A69A6CC68A14C19D8BA7FB9DBAA56554D/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-            "https://steamuserimages-a.akamaihd.net/ugc/1662356550778918358/D9BAB1849EBA7EC834BE8803A50C13FA3624F0B4/?imw=2048&imh=2048&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true",
-            "https://steamuserimages-a.akamaihd.net/ugc/1689373922260238270/032A6DA2EC7F8D310E62F2D397591F83EF40D460/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-            "https://steamuserimages-a.akamaihd.net/ugc/1689373781825653324/43B40D1971E2FC6CD50FBC6F62ED13E1476A2FBF/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-            "https://steamuserimages-a.akamaihd.net/ugc/1689373922258098329/54E0C6DE0B6526E8B5C2C528EECAC0E0E9608C82/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-        ]
-    },
-    {
-        name: "3d-modelleme-renderlar",
-        heading: "3D Modelleme/Renderlar",
-        contents: [
-            "./assets/nano-dark.webp",
-            "./assets/ard-mega-dark.webp",
-            "./assets/room.webp",
-            "./assets/finallowres.gif",
-            "./assets/skull.webp",
-            "./assets/delorean-arka.webp",
-            "./assets/mustang-on.webp",
-        ]
-    }
-];
+
+// pages are from "./datasource.js"
+
 const createDetailedGallery = (gallery) => {
     let result = `<div id="content-highlight">
             <div style="background-image:url('${gallery.contents[0].background}')" id="highlight-background" class="highlight-background"></div>
             <div id="highlight-details">
-                <h3 id="highlight-name">${gallery.contents[0].name}</h3>
-                <p id="highlight-summary">${gallery.contents[0].summary}</p>
+                <h3 id="highlight-name">${gallery.contents[0].name[currentLanguage]}</h3>
+                <p id="highlight-summary">${gallery.contents[0].summary[currentLanguage]}</p>
                 <div id="highlight-actions">
-                    ${gallery.contents[0].actions.map(action => `<a target="_blank" href="${action.link}">${action.name}</a>`).join("\n")}
+                    ${gallery.contents[0].actions.map(action => `<a target="_blank" href="${action.link}">${action.name[currentLanguage]}</a>`).join("\n")}
                 </div>
             </div>
         </div>\n<div id="more-content">\n`
-    // gallery.contents.shift();
     gallery.contents.forEach((content, index) => {
         result += `<div class="mini-content"><div onclick="changeHighlight(this)" data-index="${index}" data-parent-name="${gallery.name}" style="background:url('${content.background.replace('assets/', 'assets/proxy/')}') no-repeat center;background-size: cover;width:100%;height:100%;"></div></div>\n`
     })
@@ -165,11 +45,10 @@ const createDetailedGallery = (gallery) => {
 }
 const createGallery = (gallery) => {
     let result = `<div id="content-highlight">
-            <div style="background-image:url('${gallery.contents[0]}')" id="highlight-background" class="highlight-background"></div>
+            <div style="background-image:url('${gallery.contents[0]['raw']}')" id="highlight-background" class="highlight-background"></div>
         </div>\n<div id="more-content">\n`
-    // gallery.contents.shift();
     gallery.contents.forEach(content => {
-        result += `<div class="mini-content"><div onclick="changeHighlight(this)" data-background="${content.replace('assets/', 'assets/proxy/')}" class="highlight-background" style="background-image:url('${content}')"></div></div>\n`
+        result += `<div class="mini-content"><div onclick="changeHighlight(this)" data-background="${content['raw']}" class="highlight-background" style="background-image:url('${content['proxy'] ? content['proxy'] : content['raw']}')"></div></div>\n`
     })
     result += "</div>"
     return result;
@@ -185,9 +64,9 @@ function changeHighlight(element) {
         const galleryHighlightName = document.getElementById('highlight-name');
         const galleryHighlightSummary = document.getElementById('highlight-summary');
         const galleryHighlightActions = document.getElementById('highlight-actions');
-        galleryHighlightName.innerText = selectedPage.contents[selectedIndex].name;
-        galleryHighlightSummary.innerText = selectedPage.contents[selectedIndex].summary;
-        galleryHighlightActions.innerHTML = selectedPage.contents[selectedIndex].actions.map(action => `<a target="_blank" href="${action.link}">${action.name}</a>`).join("\n");
+        galleryHighlightName.innerText = selectedPage.contents[selectedIndex].name[currentLanguage];
+        galleryHighlightSummary.innerText = selectedPage.contents[selectedIndex].summary[currentLanguage];
+        galleryHighlightActions.innerHTML = selectedPage.contents[selectedIndex].actions.map(action => `<a target="_blank" href="${action.link}">${action.name[currentLanguage]}</a>`).join("\n");
         galleryHighlight.style.backgroundImage = `url('${selectedPage.contents[selectedIndex].background}')`;
     }
 }
@@ -196,15 +75,18 @@ function loadContent(contentType, contentName) {
     switch (contentType) {
         case "detailed-gallery":
             pages.find(page => page.name === contentName) ? contentDetails.innerHTML = createDetailedGallery(pages.find(page => page.name === contentName)) : console.error("Sayfa bulunamadı!");
-            contentHeading.innerText = pages.find(page => page.name === contentName).heading;
+            activePageName = contentName;
+            contentHeading.innerText = pages.find(page => page.name === contentName).heading[currentLanguage];
             break;
         case "gallery":
             pages.find(page => page.name === contentName) ? contentDetails.innerHTML = createGallery(pages.find(page => page.name === contentName)) : console.error("Sayfa bulunamadı!");
-            contentHeading.innerText = pages.find(page => page.name === contentName).heading;
+            activePageName = contentName;
+            contentHeading.innerText = pages.find(page => page.name === contentName).heading[currentLanguage];
             break;
         case "custom":
             pages.find(page => page.name === contentName) ? contentDetails.innerHTML = pages.find(page => page.name === contentName).content : console.error("Sayfa bulunamadı!");
-            contentHeading.innerText = pages.find(page => page.name === contentName).heading;
+            activePageName = contentName;
+            contentHeading.innerText = pages.find(page => page.name === contentName).heading[currentLanguage];
             break;
         default:
             console.error("İçerik tipi tanınmıyor!");
@@ -212,79 +94,47 @@ function loadContent(contentType, contentName) {
     }
 }
 
-var isBackgroundAnimationEnabled = true;
 
+// Background Related
+var isBackgroundAnimationEnabled = true;
 const dipsMap = document.getElementById('dispMap');
 const turbulence = document.getElementById('turbulence');
-const animationCheckbox = document.getElementById('bgAnimationCheckbox');
 
+const animationCheckbox = document.getElementById('bgAnimationCheckbox');
 animationCheckbox.addEventListener('change', () => {
     isBackgroundAnimationEnabled = !isBackgroundAnimationEnabled;
-})
+});
 
 setInterval(async () => {
-    if(isBackgroundAnimationEnabled && document.hasFocus()) turbulence.setAttribute('baseFrequency', Math.random());
-}, 15000);
-
-const themes = [
-    {
-        name: 'darkside',
-        data: {
-            hoverBorderColor: 'white',
-            unfocusedBorderColor: '#ffffff80',
-            backgroundColor: 'black',
-            patternColor: 'white',
-            textAssist: false,
-        }
-    },
-    {
-        name: 'flashbang',
-        data: {
-            hoverBorderColor: 'black',
-            unfocusedBorderColor: '#646464',
-            backgroundColor: 'white',
-            patternColor: 'black',
-            textAssist: true,
-        }
-    },
-    {
-        name: 'sepia',
-        data: {
-            hoverBorderColor: '#f1ab75',
-            unfocusedBorderColor: '#a57550',
-            backgroundColor: '#492723',
-            patternColor: '#eadbcb',
-            textAssist: false,
-        }
-    },
-    {
-        name: 'neo',
-        data: {
-            hoverBorderColor: '#00FF41',
-            unfocusedBorderColor: '#008F11',
-            backgroundColor: '#0D0208',
-            patternColor: '#00FF41',
-            textAssist: false,
-        }
-    },
-    {
-        name: 'bios',
-        data: {
-            hoverBorderColor: '#a9a9a9',
-            unfocusedBorderColor: '#5c5c5c',
-            backgroundColor: '#00005d',
-            patternColor: 'white',
-            textAssist: false,
-        }
-    }
-]
-
-document.querySelectorAll('.theme-select').forEach(element => element.addEventListener('click', () => {
-    const selectorValue = element.getAttribute('data-name');
-    const selectedTheme = themes.find(theme => theme.name === selectorValue);
+    // Add && document.hasFocus() to only change background when the page is visible
+    if(isBackgroundAnimationEnabled) turbulence.setAttribute('baseFrequency', Math.random());
+}, 20000);
+// themes from "datasource.js"
+function changeTheme(themeName) {
+    const selectedTheme = themes.find(theme => theme.name === themeName);
     document.documentElement.style.setProperty('--unfocused-border-color', selectedTheme.data.unfocusedBorderColor);
     document.documentElement.style.setProperty('--hover-border-color', selectedTheme.data.hoverBorderColor);
     document.documentElement.style.setProperty('--background-color', selectedTheme.data.backgroundColor);
     document.documentElement.style.setProperty('--pattern-color', selectedTheme.data.patternColor);
     document.body.style.color = selectedTheme.data.textAssist ? 'black' : 'white';
-}))
+}
+changeTheme('neo');
+
+document.querySelectorAll('.theme-select').forEach(element => element.addEventListener('click', () => changeTheme(element.getAttribute('data-name'))));
+
+// Language Related
+var currentLanguage = "tr";
+var activePageName = "projelerim";
+
+function changeLanguage(languageName) {
+    currentLanguage = languageName;
+    currentPage = pages.find(page => page.name === activePageName);
+    loadContent(currentPage.type, activePageName)
+}
+document.querySelectorAll('.language-select').forEach(element => element.addEventListener('click', () => changeLanguage(element.getAttribute('data-name'))));
+
+/*
+TODO:
+    * Add language support of the left side div
+    * Change background even if the user is not focused on the page (if it hasn't been a long time since they unfocused)
+*/
---
+++ datasource.js
@@ -0,0 +1,209 @@
+const pages = [
+    {
+        name:"projelerim",
+        type:"detailed-gallery",
+        heading: {eng:"My Projects", tr:"Projelerim"},
+        contents: [
+            {
+                background: './assets/musicflake.webp',
+                name: {eng:"Musicflake", tr:"Musicflake"},
+                summary: {
+                    eng:"An Angular project that uses Open-Mateo.com's API to recommend music to it's users based on their location.",
+                    tr:"Open-Mateo.com'un API'ı ile konumunuzdaki hava durumunu tespit edip hava durumuna göre Spotify'dan çalma listesi öneren bir Angular projesi."
+                },
+                actions: [
+                    {
+                        name: {eng:"Source Code", tr:"Kaynak Kod"},
+                        link: "https://github.com/yussufbiyik/musicflake"
+                    },
+                    {
+                        name: {eng:"Demo",tr:"Demo"},
+                        link: "https://musicflake-3f0a8.web.app/"
+                    }
+                ]
+            },
+            {
+                background:'./assets/otoroblox.webp',
+                name:{eng:"OtoRoblox", tr:"OtoRoblox"},
+                summary: {
+                    eng:"Reward distribution software of a mobile app that gives in-game rewards to users. Uses an API that wraps Roblox API to send the rewards to it's users.",
+                    tr:"Kullanıcılarının oyun içi ödüller kazandığı bir uygulamanın ödül dağıtım yazılımıdır, bir API ile oyundaki (Roblox) görevlerini tamamlayanlara ödüllerini verir."
+                },
+                actions: []
+            },
+            {
+                background:'./assets/periyodik-tablo.webp',
+                name:{eng:"Periodic Table", tr:"Periyodik Tablo"},
+                summary: {
+                    eng:"MVP of a mobile app that I made for a costumer. I adapted this version to Angular then converted it to a mobile app using Cordova.",
+                    tr:"Bir müşterim için sonradan geliştirdiğim bir mobil uygulamanın basit bir prototipi, sonraki aşamada prototipi Angular'a uyarlayıp Cordova ile bir mobil uygulamaya çevirdim."
+                },
+                actions: [
+                    {
+                        name: {eng:"Source Code", tr:"Kaynak Kod"},
+                        link: "https://github.com/yussufbiyik/periodic-table"
+                    },
+                    {
+                        name: {eng:"Demo",tr:"Demo"},
+                        link: "https://yussufbiyik.github.io/periodic-table/"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: "yetkinliklerim",
+        type:"custom",
+        heading: {eng:"Skills", tr:"Yetkinliklerim"},
+        content: `
+            <div class="skills">
+                <div id="software-related" class="skill">
+                    <ul>
+                        <li>
+                            <span>Web Geliştirme // Web Development</span>
+                            <ul>
+                                <li>
+                                    <span>HTML & CSS</span>
+                                    <ul>UIkit</ul>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <span>Javascript & Typescript</span>
+                                    <ul>NodeJS, Angular, Electron, React Native</ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>Python</li>
+                        <li>Arduino</li>
+                        <li>Firebase</li>
+                        <li>SQL</li>
+                    </ul>
+                </div>
+                <div id="general-skills" class="skill">
+                    <ul>
+                        <li>
+                            <span>3D</span>
+                            <ul>
+                                <li>Cinema 4D (Modelleme ve Animasyon)</li>
+                                <li>Tinkercad</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <span>2D</span>
+                            <ul>
+                                <li>Photoshop</li>
+                                <li>Aseprite (Pixel Art)</li>
+                                <li>Video Düzenleme</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>`
+    },
+    {
+        name: "pixel-art",
+        type:"gallery",
+        heading: {eng:"Pixel Art", tr:"Pixel Art"},
+        contents: [
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1689373781825613769/6D86A60AD7424F0D53E00E1420C3E3D3AE3CE327/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false", proxy:false},
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1689373781825653324/43B40D1971E2FC6CD50FBC6F62ED13E1476A2FBF/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false", proxy:false},
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1633076066618028298/F53768ED0B0B5CA8852B43C3F498204B7C39693C/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false", proxy:false},
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1474317354726727080/3AA259DF9EA002FB45D128CB02B185BADD7FEA40/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false", proxy:false},
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1689373781826880823/189E3DBD5E9E0DEF1C3BAB1112722EB9327E6DDD/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false", proxy:false},
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1813239499466346854/614AD40A4B7E3B08EF7AFBCD5149C87CC18DD630/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false", proxy:false},
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1691625928964912516/F6F49F0A69A6CC68A14C19D8BA7FB9DBAA56554D/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false", proxy:false},
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1662356550778918358/D9BAB1849EBA7EC834BE8803A50C13FA3624F0B4/?imw=2048&imh=2048&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true", proxy:false},
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1689373922260238270/032A6DA2EC7F8D310E62F2D397591F83EF40D460/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false", proxy:false},
+            {raw:"https://steamuserimages-a.akamaihd.net/ugc/1689373922258098329/54E0C6DE0B6526E8B5C2C528EECAC0E0E9608C82/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false", proxy:false},
+        ]
+    },
+    {
+        name: "3d-modelleme-renderlar",
+        type:"gallery",
+        heading: {eng:"3D Models/Renders",tr:"3D Modelleme/Renderlar"},
+        contents: [
+            {
+                raw:"https://raw.githubusercontent.com/yussufbiyik/yussufbiyik.github.io/main/assets/room.webp",
+                proxy:"assets/proxy/room.webp"
+            },
+            {
+                raw:"https://github.com/yussufbiyik/yussufbiyik.github.io/blob/main/assets/finallowres.gif?raw=true",
+                proxy: false
+            },
+            {
+                raw:"https://raw.githubusercontent.com/yussufbiyik/yussufbiyik.github.io/main/assets/ard-mega-dark.webp",
+                proxy:"assets/proxy/ard-mega-dark.webp"
+            },
+            {
+                raw:"https://raw.githubusercontent.com/yussufbiyik/yussufbiyik.github.io/main/assets/nano-dark.webp",
+                proxy:"assets/proxy/nano-dark.webp"
+            },
+            {
+                raw:"https://raw.githubusercontent.com/yussufbiyik/yussufbiyik.github.io/main/assets/skull.webp",
+                proxy:"assets/proxy/skull.webp"
+            },
+            {
+                raw:"https://raw.githubusercontent.com/yussufbiyik/yussufbiyik.github.io/main/assets/delorean-arka.webp",
+                proxy:"assets/proxy/delorean-arka.webp"
+            },
+            {
+                raw:"https://raw.githubusercontent.com/yussufbiyik/yussufbiyik.github.io/main/assets/mustang-on.webp",
+                proxy:"assets/proxy/mustang-on.webp"
+            },
+        ]
+    }
+]
+
+const themes = [
+    {
+        name: 'darkside',
+        data: {
+            hoverBorderColor: 'white',
+            unfocusedBorderColor: '#ffffff80',
+            backgroundColor: 'black',
+            patternColor: 'white',
+            textAssist: false,
+        }
+    },
+    {
+        name: 'flashbang',
+        data: {
+            hoverBorderColor: 'black',
+            unfocusedBorderColor: '#646464',
+            backgroundColor: 'white',
+            patternColor: 'black',
+            textAssist: true,
+        }
+    },
+    {
+        name: 'sepia',
+        data: {
+            hoverBorderColor: '#f1ab75',
+            unfocusedBorderColor: '#a57550',
+            backgroundColor: '#492723',
+            patternColor: '#eadbcb',
+            textAssist: false,
+        }
+    },
+    {
+        name: 'neo',
+        data: {
+            hoverBorderColor: '#00FF41',
+            unfocusedBorderColor: '#008F11',
+            backgroundColor: '#0D0208',
+            patternColor: '#00FF41',
+            textAssist: false,
+        }
+    },
+    {
+        name: 'bios',
+        data: {
+            hoverBorderColor: '#a9a9a9',
+            unfocusedBorderColor: '#5c5c5c',
+            backgroundColor: '#00005d',
+            patternColor: 'white',
+            textAssist: false,
+        }
+    }
+]
